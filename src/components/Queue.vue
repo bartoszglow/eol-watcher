1e150444969226a800351db36df8ea646801eb85
Bartosz Glowacki <bartosz.glow@gmail.com>
2019-01-27 13:14:57 +0100

    feat(queue): remaining time for in progress battle

--- src/components/Queue.vue
+++ src/components/Queue.vue
@@ -4,7 +4,13 @@
       <img class="img-circle media-object pull-left" :src="isInQueue(battle) ? 'assets/queue.png' : `http://elmaonline.net/images/map/${battle.level}`">
       <div class="media-body">
         <h2 class="battle-title"><strong>{{battle.levelname}}</strong> <small><i>by</i></small> <strong>{{battle.kuski}}</strong></h2>
-        <p class="battle-description"><i>time:</i> <strong>{{battle.duration}} minutes</strong></p>
+        <p class="battle-description">
+          <i>time:</i>
+          <strong>
+            <span v-if="isInProgress(battle)">{{remainingTime}} of</span>
+            {{battle.duration}} minutes
+          </strong>
+        </p>
         <p class="battle-description"><i>type:</i> <strong>{{battle.type}}</strong></p>
       </div>
       <div class="media-object pull-right status-icon">
@@ -15,8 +21,20 @@
 </template>
 
 <script>
+  import moment from 'moment'
+
   export default {
     name: 'queue',
+    data: function() {
+      return {
+        remainingTime: '',
+        timer: ''
+      }
+    },
+    created: function() {
+      this.setRemainingTime();
+      this.timer = setInterval(this.setRemainingTime, 1000)
+    },
     methods: {
       isInQueue: function(battle) {
         return battle.inqueue === 1
@@ -26,7 +44,20 @@
       },
       isFinished: function(battle) {
         return battle.finished === 1
+      },
+      setRemainingTime: function() {
+        const battle = this.$store.state.battles.all.find(battle => this.isInProgress(battle))
+        if(battle) {
+          const serverToUTC = 3600 * 9
+          const secondsGap = 23
+          const timezoneDiff = (new Date()).getTimezoneOffset() * 60
+          const dateDiff = moment().diff(moment(new Date((battle.started - serverToUTC + timezoneDiff - secondsGap) * 1000)))
+          this.remainingTime = moment.utc(Math.round(battle.duration * 60 - dateDiff / 1000) * 1000).format('mm:ss')
+        }
       }
+    },
+    beforeDestroy() {
+      clearInterval(this.timer)
     }
   }
 </script>
@@ -40,6 +71,9 @@
   }
   .media-body {
     padding-left: 5px;
+  }
+  .list-group-item {
+    position: relative;
   }
   .status-icon {
     position: absolute;
